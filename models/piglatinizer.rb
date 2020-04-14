d408c0f5d3d8e8f76d6a6be7d7cd643f702b9e79
Tammy Lam <lamtammy1515@gmail.com>
2020-04-14 23:51:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,24 +1,37 @@
-
 class PigLatinizer
-  attr_accessor :piglatinized_phrase
 
-def piglatinize(word) #pig latinizes a single word
-  if word.downcase.index(/[aeiou]/) == 0
-    word + "way"
-  else
-    vowel_index = word.index(/[aeiou]/)
-    front_end = word.slice!(0..vowel_index-1)
-    word + front_end +"ay"
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
-end
 
-def to_pig_latin(phrase)
-  word_array = phrase.split(" ")
-  @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
-end
+  private
 
-  def to_pig_latin(string)
-    string.split.collect{|word| piglatinize(word)}.join(" ")
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
   end
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+
+
 
 end
