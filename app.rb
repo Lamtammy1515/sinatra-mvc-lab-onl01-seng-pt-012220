fbfd7a8385674ea5bfbeea518d7c48c79bfcd8ad
Tammy Lam <lamtammy1515@gmail.com>
2020-04-14 23:47:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input 
+  end 
+  
+   post '/piglatinize' do
+    @putinay_input = PigLatinizer.new.to_pig_latin(params[:phrase])
+    erb :result
+  end
 end
